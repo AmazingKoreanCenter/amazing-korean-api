03d409260a38f84f6252b95cf50c061d32f5bb80
AmazingKoreanCenter <amazingkoreancenter@gmail.com>
2025-08-27 14:23:00 +0900

    DB 정리 및 업데이트

--- migrations/20250821005843_users_birthday_to_date.sql
+++
@@ -1,9 +0,0 @@
--- Change users.user_birthday TIMESTAMPTZ -> DATE
-
---! up
-ALTER TABLE users
-  ALTER COLUMN user_birthday TYPE DATE USING (user_birthday::date);
-
---! down
-ALTER TABLE users
-  ALTER COLUMN user_birthday TYPE TIMESTAMPTZ USING (user_birthday::timestamptz);
--- migrations/20250821011843_users_birthday_to_date_fix.sql
+++
@@ -1,7 +0,0 @@
---! up
-ALTER TABLE public.users
-  ALTER COLUMN user_birthday TYPE DATE USING (user_birthday::date);
-
---! down
-ALTER TABLE public.users
-  ALTER COLUMN user_birthday TYPE TIMESTAMPTZ USING (user_birthday::timestamptz);
